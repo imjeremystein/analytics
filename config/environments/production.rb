186e852272d6df1a716821a791640964106a7a4d
Jeremy Stein <jsstein@gmail.com>
2014-01-22 20:09:44 -0500

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,9 @@ Analytics::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #required for heroku
+  #set this to the actual host name
+  config.action_mailer.default_url_options = { :host => 'halfpast-analytics.herokuapp.com' }
+
 end
